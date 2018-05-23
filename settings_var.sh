91275789beca253bbfaa3b792c5a2c7f6ee3bf6e
shedprog <flynik75@gmail.com>
2018-05-23 20:23:45 +0300

    limits

--- settings_var.sh
+++ settings_var.sh
@@ -1,6 +1,6 @@
 # All global variables and enviroments
 export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export OUTPUTDIR="/nfs/dust/zeus/group/mykytaua/OUTPUT_X6"
+export OUTPUTDIR="/nfs/dust/zeus/group/mykytaua/ALL14"
 #export OUTPUTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # models=( "RR" "LL" "VV" "VA" "AA" "X1" "X2" "X3" "X4" )
 models=( "X6" )
